65928fdfe2d5f00df98a84ae4e8a49b2c00a8160
PeVe91 <321365708+PeVe91@users.noreply.github.com>
2026-09-11 10:43:05 +0200

    Test

---
+++ src/main/java/org/example/Uppg1_1.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Uppg1_1 {
+    static void main() {
+
+    }
+
+    private class Product {
+
+        private String name;
+        private double price;
+        private int stockQuantity = 0;
+    }
+}
